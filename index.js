0c6f2a2571809c2689bc9b777305ad721d733ffd
saidureg <saidureg@gmail.com>
2023-12-22 19:40:20 +0600

    deploy in vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const taskCollection = client.db("taskManageDB").collection("task");
 
     // task related api
@@ -34,7 +34,7 @@ async function run() {
 
     app.get("/tasks/user/:email", async (req, res) => {
       const email = req.params.email;
-      const query = { OwnerEmail: email };
+      const query = { email: email };
       const result = await taskCollection.find(query).toArray();
       res.send(result);
     });
@@ -70,10 +70,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
